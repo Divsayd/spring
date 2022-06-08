fac201882197ac4b99ced2db259ea082cdfc845f
Divsayd <107132042+Divsayd@users.noreply.github.com>
2022-06-08 11:29:38 -0400

    Add files via upload

---
+++ ACcontrol.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ACcontrol {
+
+    private String status;
+    private int temp;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String title) {
+        this.status = title;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+}
